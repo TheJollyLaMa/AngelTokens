8b5ec0efba38e5554e157ae0b3cd855ba51b5c43
The Jolly LaMa <j@caffeinelamanna.com>
2020-12-06 11:18:43 -0500

    added contract abi to controller

--- js/webApp.js
+++ js/webApp.js
@@ -24,29 +24,63 @@ app.config([
     .otherwise({redirectTo: "/"})
   }
 ]);
-app.controller("StoreFrontController", ["$scope", "$window", function ($scope, $window) {
-
+app.controller("StoreFrontController", ["$scope", function ($scope, $window) {
     $scope.test = "testing data binding in the store front";
-    $scope.getBalance = function () {
-        var address, wei, balance;
-        address = document.getElementById("address").value;
-        $scope.address = address;
-        try {
-            web3.eth.getBalance(address, function (error, wei) {
-                if (!error) {
-                    var balance = web3.fromWei(wei, 'ether');
-                    document.getElementById("output").innerHTML = balance + " ETH";
-                }
-            });
-        } catch (err) {
-            document.getElementById("output").innerHTML = err;
-        }
-    }
-
 }]);
 app.controller("BehindTheCounterController", ["$scope", function ($scope) {
     $scope.test = "testing data binding behind the counter";
 }]);
-app.controller("AngelsRoomController", ["$scope", function ($scope) {
+app.controller("AngelsRoomController", ["$scope", "$window", "$http", function ($scope, $window, $http) {
      $scope.test = "testing data binding in the Angel's Room";
+
+     $http.get('abis/Color.json').then(function(c) {
+       $scope.contract = c.data;
+       // console.log($scope.contract);
+       $scope.loadTheBlock();
+     });
+
+     $scope.loadTheBlock = function () {
+       const web3 = window.web3
+       //load account from metamask
+       web3.eth.getAccounts().then(function(accounts){
+          $scope.account = accounts[0];
+       });
+       web3.eth.net.getId().then(function(net_id){
+          $scope.networkId = net_id;
+          console.log(net_id);
+          $scope.networkData = $scope.contract.networks[$scope.networkId];
+          console.log($scope.networkData);
+
+          if($scope.networkData) {
+            const abi = $scope.contract.abi;
+            const address = $scope.networkData.address;
+            $scope.ColorsContract = new web3.eth.Contract(abi,address);
+            $scope.totalSupply = $scope.ColorsContract.methods.totalSupply().call();
+            // load colors
+            for (var i = 1; i <= $scope.totalSupply; i++) {
+              $scope.color[i] = $scope.ColorsContract.methods.colors(i - 1).call();
+            }
+          }else {
+            $window.alert("Smart contract not connected to selected network.")
+          }
+        });
+
+      }
+
+     $scope.getBalance = function () {
+         var address, wei, balance;
+         address = document.getElementById("address").value;
+         console.log(address);
+         $scope.address = address;
+         try {
+             web3.eth.getBalance(address, function (error, wei) {
+                 if (!error) {
+                     var balance = web3.utils.fromWei(wei, 'ether');
+                     document.getElementById("output").innerHTML = balance + " ETH";
+                 }
+             });
+         } catch (err) {
+             document.getElementById("output").innerHTML = err;
+         }
+     }
 }]);
