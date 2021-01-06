d1d7dd913a6389e8b091c18ed292efe9eb39fcf2
TheJollyLaMa <j@caffeinelamanna.com>
2021-01-05 22:25:09 -0500

    attempted deployment to rinkeby with Infura

--- truffle.js
+++ truffle.js
@@ -1,11 +1,21 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*", // Match any network id
-      from: "0xc264efeD9e9983E3992ee8270D3A3bf9306E9f31" //dummy account in ganache for now but should be set to the angel deployer wallet for mainnet deployment
-    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "*", // Match any network id
+    //   from: process.env.GDA //dummy account in ganache for now but should be set to the angel deployer wallet for mainnet deployment
+    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 6500000,
+      gasPrice: 75000000000,
+
+    }
   },
   contracts_directory: './contracts/',
   contracts_build_directory: './abis/',
